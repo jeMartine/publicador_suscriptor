3435f86e1b45668d0fe6a0f3cb1719a01f7022a9
jeMartine <jorgeesteban464@gmail.com>
2025-10-05 13:42:57 -0500

    Feature: Adding FE+BK to subscriber and Bk to publisher

--- publisher/app_publisher/src/models/Turn.ts
+++ publisher/app_publisher/src/models/Turn.ts
@@ -18,14 +18,14 @@ export class Turn {
         { id, user, operation, assigned_to, turn_in_line, letter, state, creationDate}:
         {id: string, user: User, operation: Operation, assigned_to: Assessor, turn_in_line:number, letter:string, state:number, creationDate:Date}
     ){
-    this.id = id;                   // UUID o contador
-    this.user = user;         // Objeto Cliente
-    this.operation = operation;       // Objeto Servicio
-    this.assigned_to = assigned_to;           // Número de turno (01, 02, …)
-    this.turn_in_line = turn_in_line;             // Letra adicional si hay más de 99
-    this.letter = letter;           // 1: EN_ESPERA |2: LLAMADO | 3: ATENDIENDO | 4: FINALIZADO
-    this.state = state;     // ID o nombre del asesor
-    this.creationDate = new Date();
+    this.id = id;            
+    this.user = user;                 // Objeto Cliente
+    this.operation = operation;       // Objeto Operación
+    this.assigned_to = assigned_to;   // Asignación de operario
+    this.turn_in_line = turn_in_line; // Turno en la fila
+    this.state = 1;                   // 1: EN_ESPERA |2: LLAMADO | 3: ATENDIENDO | 4: FINALIZADO
+    this.letter = "";                 // Letra adicional si superan los 99 clientes en fila
+    this.creationDate = new Date();   // fecha de creación
   }
 
   // Formato completo: TG01, SD01, TGA01, etc.
